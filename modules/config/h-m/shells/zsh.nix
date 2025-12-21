d201922ac5e06f520e2c2227978fb992ea393c36
xvr6 <xvr0612@gmail.com>
2025-12-21 18:37:04 -0500

    shell fontsize change

--- modules/config/h-m/shells/zsh.nix
+++ modules/config/h-m/shells/zsh.nix
@@ -26,36 +26,37 @@
 		enable = true;
 		themes = {
 			tokyo-night-default = {
-        background = "#1a1b26";
-        foreground = "#c0caf5";
-        selection-background = "#283457";
-        selection-foreground = "#c0caf5";
-        cursor-color = "#c0caf5";
-        cursor-text = "#1a1b26";
-        palette = [
-            "0=#15161e"
-            "1=#f7768e"
-            "2=#9ece6a"
-            "3=#e0af68"
-            "4=#7aa2f7"
-            "5=#bb9af7"
-            "6=#7dcfff"
-            "7=#a9b1d6"
-            "8=#414868"
-            "9=#f7768e"
-            "10=#9ece6a"
-            "11=#e0af68"
-            "12=#7aa2f7"
-            "13=#bb9af7"
-            "14=#7dcfff"
-            "15=#c0caf5"
-        ];
+  	      	background = "#1a1b26";
+        	foreground = "#c0caf5";
+	        selection-background = "#283457";
+        	selection-foreground = "#c0caf5";
+       		cursor-color = "#c0caf5";
+        	cursor-text = "#1a1b26";
+        	palette = [
+	            "0=#15161e"
+        	    "1=#f7768e"
+       		    "2=#9ece6a"
+                    "3=#e0af68"
+ 	            "4=#7aa2f7"
+        	    "5=#bb9af7"
+        	    "6=#7dcfff"
+       	            "7=#a9b1d6"
+                    "8=#414868"
+	            "9=#f7768e"
+	            "10=#9ece6a"
+	            "11=#e0af68"
+	            "12=#7aa2f7"
+	            "13=#bb9af7"
+	            "14=#7dcfff"
+	            "15=#c0caf5"
+	        ];
 			};
 		};
 
 
 		settings = {
 			theme = "tokyo-night-default";
+			font-size = 10;
 		};
 
 	};
